25641ffb62d6e12db9ff1f94deebd29588587214
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-03-31 11:23:08 +0100

    feat: completed&edit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,31 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Todo = ({todo, todos, setTodos,id}) => {
+const Todo = ({ todo, todos, setTodos, id }) => {
 
-    const onDelete = () => {
-        setTodos(todos.filter((item) => 
-        todo.id !== item.id))
+    const [edit, setEdite] = useState(false);
+    const [inputedite, setInputedite] = useState(todo.name);
+
+    const onEdite = () => {
+        setEdite(true);
     }
-    const onchangedit = () => {}
-  return (
-    <div className='todo-li'>
-        <li className="list-li">
-            <input className='li-input' value={todo.name} onChange={onchangedit}/>
-            <button className='button-complete'>
-                <span className='text-complete'>Complete</span><i className='fa fa-check'></i>
-            </button>
+    const onComplete = () => {
+        setTodos(todos.map((item) => {
+            if (todo.id === item.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+    const onDelete = () => {
+        setTodos(todos.filter((item) =>
+            todo.id !== item.id))
+    }
+    const onchangedit = () => {
 
-            <button className='button-edit'>
-                <span className='text-edit'>Edit</span><i className='fa fa-edit'></i>
-            </button>
+        if (edit) {
+            
+        } else {
+            return (
+                <div className='todo-li'>
+                    <li className={`list-li ${todo.completed ? 'completed' : ''}`}>
+                        <input className='li-input' value={todo.name} onChange={onchangedit} />
+                        <button className='button-complete' onClick={onComplete}>
+                            <span className='text-complete'>Complete</span><i className='fa fa-check'></i>
+                        </button>
 
-            <button className='button-delete' onClick={onDelete}>
-                <span className='text-delete'>Delete</span><i className='fa fa-trash'></i>
-            </button>
-        </li>
-      
-    </div>
-  )
+                        <button className='button-edit' onClick={onEdite}>
+                            <span className='text-edit'>Edit</span><i className='fa fa-edit'></i>
+                        </button>
+
+                        <button className='button-delete' onClick={onDelete}>
+                            <span className='text-delete'>Delete</span><i className='fa fa-trash'></i>
+                        </button>
+                    </li>
+
+                </div>
+            )
+        }
+    }
+
 }
 
 export default Todo
